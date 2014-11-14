6a8c2d37095537a46955f7874538faf5b84b7d29
crackerhead <nbower@mac.com>
2014-11-13 18:37:53 -0800

    Added navbars

---
+++ js/_custom.js
@@ -0,0 +1,13 @@
+// Slide in second navbar START
+$(function() {
+	var bar = $('#headerSlideContainer');
+	var top = bar.css('top');
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 250) {
+			bar.stop().animate({'top' : '0px'}, 400);
+		} else {
+			bar.stop().animate({'top' : top}, 0);
+		}
+	});
+});
+// Slide in second navbar END
