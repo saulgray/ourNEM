9a56aa001337f803ca6afd509da3af2ca65eec7e
crackerhead <nbower@mac.com>
2014-11-15 17:06:53 -0800

    all sections but NCC modified for mobile

--- js/_custom.js
+++ js/_custom.js
@@ -3,7 +3,7 @@ $(function() {
 	var bar = $('#headerSlideContainer');
 	var top = bar.css('top');
 	$(window).scroll(function() {
-		if($(this).scrollTop() > 250) {
+		if($(this).scrollTop() > 120) {
 			bar.stop().animate({'top' : '0px'}, 400);
 		} else {
 			bar.stop().animate({'top' : top}, 0);
